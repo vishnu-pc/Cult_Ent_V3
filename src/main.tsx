4cc86b423f69b2b6d1368eb3a3e03494e9f87941
vishnu-pc <vishnupc1999@gmail.com>
2025-06-30 15:46:09 +0530

    Temp Git Changes Removes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App'
-// Temp Declaration
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
